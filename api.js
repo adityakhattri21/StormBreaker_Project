487a406b24eb659e86cf748ccd8726b518612c61
Aditya Khattri <adityakhattri123@gmail.com>
2023-01-08 23:20:12 +0530

    Added CORS policy

---
+++ api.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const axios = require('axios');
+
+
+
+
+const api = express();
+
+axios.defaults.timeout = 100000;
+
+api.get("/devices",(req,res)=>{
+    axios.get('https://raw.githubusercontent.com/adityakhattri21/StormBreaker_Project/master/devices.json')
+    .then(response =>{
+        // res.send(response.data.devices[0].name)
+        res.send(response.data)
+    })
+    // res.status(200).json({'message':'Hi biatch'} );
+});
+
+api.get("/core",(req,res)=>{
+    axios.get("https://raw.githubusercontent.com/stormbreaker-project/official_devices/master/team/core.json")
+    .then(response=>{
+        res.send(response.data);
+    })
+});
+
+api.get("/maintainers" , (req,res)=>{
+    axios.get('https://raw.githubusercontent.com/stormbreaker-project/official_devices/master/team/maintainers.json')
+    .then(response =>{
+        res.send(response.data);
+    })
+})
+
+
+module.exports = api;
