68ad6e8029eadd06cfcba8d84c1d3fcd8fc8bc1b
Aditya Khattri <adityakhattri123@gmail.com>
2023-01-08 23:46:23 +0530

    Added CORS policy

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+const http = require('http');
+const api = require('./api');
+const cors = require('cors');
+
+api.use(cors({
+    origin: "*" //allows the data to be fetched from anywhere. 
+}))
+
+const server = http.createServer(api);
+const port = process.env.PORT || 3000 ;
 
 server.listen(port, ()=>{
-    console.log(`Server is up and running at port ${port}`);
-})
+    console.log(`Server is up and running at port ${port}`)
+});
